8c1f400cb7b0d3648ecae54c13cdb72b9e66496e
Florian Hanke <florian.hanke@gmail.com>
2009-06-18 08:51:40 +0200

    + pdf nicefying

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -10,17 +10,34 @@ class PdfController < ApplicationController
       # "Bookman Old Style", "Serifa BT", "URW Bookman L", "itc bookman", times, serif
       font "#{Prawn::BASEDIR}/data/fonts/Dustismo_Roman.ttf"
       
-      h1 book.title
-      text book.subtitle, :align => :center, :size => 20.8
-      text 'by', :align => :center
-      text book.author, :align => :center, :size => 17.6
+      # title
+      pad_top 300 do
+        horizontal_rule
+        h1 book.title
+        horizontal_rule
+      end
+      pad_top 10 do
+        text book.subtitle, :align => :center, :size => 20.8
+      end
+      pad_top 10 do
+        text 'by', :align => :center
+      end
+      pad_top 10 do
+        text book.author, :align => :center, :size => 17.6
+      end
       
-      text_options.update :align => :left
+      # content
+      start_new_page
+      text_options.update :align => :left, :size => 15
       text book.text
       
-      text_options.update :align => :center
-      text 'The End'
-      text '❦'
+      # end
+      start_new_page
+      text_options.update :align => :center, :size => 20.8
+      pad_top 350 do
+        text 'The End'
+        text '❦'
+      end
     end
     
     send_file file_name, :type => 'application/pdf'
